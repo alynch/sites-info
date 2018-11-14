f412357b95098bcf2db6c99f5af0f353c008e463
Alejandro Lynch <alynch@hopscotch.ca>
2018-11-14 16:56:33 -0500

    moved environment info to separate card in application's page

--- laravel/resources/views/applications/environments.blade.php
+++ laravel/resources/views/applications/environments.blade.php
@@ -1,18 +1,23 @@
-<h2>Environments</h2>
-
-@foreach ($environments as $environment)
-<div class="form-group row">
-    <label for="env-{{ $environment->id }}" class="col-sm-2 col-form-label">
-        {{ $environment->name }}
-    </label>
-
-    <div class="col-sm-10">
-      <input type="text" class="form-control"
-            id="env-{{ $environment->id }}"
-            name="env[{{ $environment->id }}]" 
-            value="{{ $environment->url ?: old('env.' . $environment->id) }}"/>
+<div class="card mb-3">
+    <div class="card-header">
+        <h3>Environments</h3>
     </div>
+
+    <div class="card-body">
+
+        @foreach ($environments as $environment)
+        <div class="form-group row">
+            <label for="env-{{ $environment->id }}" class="col-sm-2 col-form-label">
+                {{ $environment->name }}
+            </label>
+
+            <div class="col-sm-10">
+              <input type="text" class="form-control"
+                    id="env-{{ $environment->id }}"
+                    name="env[{{ $environment->id }}]"
+                    value="{{ $environment->url ?: old('env.' . $environment->id) }}"/>
+            </div>
+            </div>
+        @endforeach
     </div>
-@endforeach
-
-
+</div>
