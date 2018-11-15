5d57790d8d042faffb0aa35d2755af572991315d
Alejandro Lynch <alynch@hopscotch.ca>
2018-11-14 20:08:25 -0500

    added timelin in menu; tweaked style of timeline page

--- laravel/resources/views/layouts/nav.blade.php
+++ laravel/resources/views/layouts/nav.blade.php
@@ -20,6 +20,9 @@
                         <li>
                             <a class="nav-link" href="/applications">Applications</a>
                         </li>
+                        <li>
+                            <a class="nav-link" href="/timeline">Timeline</a>
+                        </li>
                     </ul>
                     @endauth
 
--- laravel/resources/views/timeline/index.blade.php
+++ laravel/resources/views/timeline/index.blade.php
@@ -13,15 +13,15 @@
 
 
     <div class="card-body">
-        <div class="range-box" style="display: flex">
+        <div class="range-box" style="margin-left: 15.6em; display: flex">
             @foreach ($months as $key => $month)
                 <span style="width: 8.333%; border-left: 1px solid #ccc; text-align: center">{{$month }}</span>
             @endforeach
         </div>
 
         @foreach ($applications as $application)
-            <div>
-                {{ $application->name }}
+            <div style="display: flex">
+                <span style="width: 12em">{{ $application->name }}</span>
                 <div class="range-box">
                     @foreach ($application->timeline as $period)
                     <div class="range" style="left: {{ $period->getRange()['start'] }}px; width: {{$period->getRange()['width']}}px;"> 
