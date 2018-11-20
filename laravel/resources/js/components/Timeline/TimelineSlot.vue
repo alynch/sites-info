7972ed4d214e997668e8ba0d24a2715ebd75547f
Alejandro Lynch <alynch@hopscotch.ca>
2018-11-19 22:42:26 -0500

    use computed properties to add style to timeline slots

--- laravel/resources/js/components/Timeline/TimelineSlot.vue
+++ laravel/resources/js/components/Timeline/TimelineSlot.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="range"
-        :style="'left:' + start + '%; width:' + width + '%;'">
-    </div>
+    <div class="range" v-bind:style="slotStyle"></div>
 </template>
 
 <script>
     export default {
 
         props: {
-            period: Object
+            period: Object,
+            background: {
+                type: String,
+                default: '#3490dc'
+            }
         },
-
 
        computed: {
             start: function() {
@@ -19,15 +20,21 @@
             width: function() {
                 let end = 100 * ((this.period.end_month - 1) *30 + (this.period.end_day - 1)) / 360 ;
                 return end - this.start;
+            },
+            slotStyle: function () {
+                return {
+                    left: this.start + '%',
+                    width: this.width + '%',
+                    background: this.background
+                }
             }
-        },
+        }
     }
 </script>
 
 <style scoped>
 .range {
     position: absolute;
-    background: #3490dc;
     height: 100%;
 }
 </style>
