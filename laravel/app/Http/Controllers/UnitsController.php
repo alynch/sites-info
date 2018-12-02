1202ba7b0064081c553ccd12845f79e883972e84
Alejandro Lynch <alynch@hopscotch.ca>
2018-12-01 20:30:31 -0500

    sort units alphabetically; remove unused methods

--- laravel/app/Http/Controllers/UnitsController.php
+++ laravel/app/Http/Controllers/UnitsController.php
@@ -24,35 +24,16 @@ class UnitsController extends Controller
      */
     public function index()
     {
-        $units = Unit::all();
+        $units = Unit::orderBy('name')->get();
 
         return view('units.index')
             ->with('units', $units);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
      * Display the specified resource.
      *
-     * @param  \App\Environments  $environment
+     * @param  \App\Unit $unit
      * @return \Illuminate\Http\Response
      */
     public function show(Unit $unit)
@@ -61,48 +42,5 @@ class UnitsController extends Controller
 
         return view('units.show')
             ->with('unit', $unit);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Environments  $environments
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Environments $environment)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Environments  $environment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Environments $environment)
-    {
-        $validatedData = $request->validate([
-            'name' => 'required',
-            'code' => 'required',
-            'sort_order' => 'required',
-        ]);
-
-        $environment->update($validatedData);
-
-        return redirect('/environments');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Environments  $environment
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Environments $environment)
-    {
-        $environment->delete();
-
-        return redirect('/environments');
     }
 }
