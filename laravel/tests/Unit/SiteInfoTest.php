af1149434a43cbe2936dc2786811676a4961ea3a
Alejandro Lynch <alynch@hopscotch.ca>
2019-01-05 17:18:33 -0500

    Add setup method for SiteInfo unit tests

--- laravel/tests/Unit/SiteInfoTest.php
+++ laravel/tests/Unit/SiteInfoTest.php
@@ -6,39 +6,42 @@ use GuzzleHttp\Client;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SiteInfoTest extends TestCase
 {
 
+    protected $mock;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->mock = $this->createMock(\GuzzleHttp\Client::class);
+    }
+
     /** @test */
     public function a_site_needs_a_valid_url()
     {
-        $mock = $this->createMock(\GuzzleHttp\Client::class);
-
         $url = "http://example.com";
-        $site = new \App\SiteInfo($url, $mock);
+        $site = new \App\SiteInfo($url, $this->mock);
         $this->assertEquals($url, $site->getUrl());
     }
 
     /** @test */
     public function a_site_fails_with_an_invalid_url()
     {
-        $mock = $this->createMock(\GuzzleHttp\Client::class);
 
         $this->withoutExceptionHandling();
         $url = '';
         $this->expectException('\Exception');
-        $site = new \App\SiteInfo($url, $mock);
+        $site = new \App\SiteInfo($url, $this->mock);
     }
 
     /** @test */
     public function get_header_info_from_site()
     {
-        $mock = $this->createMock(\GuzzleHttp\Client::class);
         $this->withoutExceptionHandling();
         $url = "https://corpora.chass.utoronto.ca";
-        $site = new \App\SiteInfo($url, $mock);
+        $site = new \App\SiteInfo($url, $this->mock);
 
         $this->assertInstanceOf(\App\SiteInfo::class, $site);
     }
