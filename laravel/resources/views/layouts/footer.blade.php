06ae75f20eebec0a66adea708ba3dc542669d259
Alejandro Lynch <alynch@hopscotch.ca>
2019-01-06 13:19:20 -0500

    Modify (c) date in footer

--- laravel/resources/views/layouts/footer.blade.php
+++ laravel/resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <div class="copy">
-            © 2018
+            © 2018-19
             IIT AppDev, University of Toronto
         </div>
     </div>
