62d830e4f3604d0ee6fb8a21630fbfab93c3fd20
Alejandro Lynch <alynch@hopscotch.ca>
2019-01-27 10:16:43 -0500

    Modify logo

--- laravel/resources/views/layouts/nav.blade.php
+++ laravel/resources/views/layouts/nav.blade.php
@@ -1,24 +1,11 @@
 <header>
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}" style="padding: 15px 10px">
-            <svg width="0" height="0" viewbox="0 0 32 32">
-                <g style="fill:none; stroke:#ccc; stroke-width:4; stroke-linecap:round; stroke-linejoin: round">
-                <circle cx="16" cy="16" r="12"/>
-                <path d="M16 16 L0 16"/>
-                </g>
-            </svg>
-            <svg width="32" height="32" viewbox="0 0 32 32">
-                <g style="fill:none; stroke:#ccc; stroke-width:4; stroke-linecap:round; stroke-linejoin: round">
-                <path d="M2 6 l 28 0"/>
-                <path d="M2 16 l 28 0"/>
-                <path d="M2 26 l 28 0"/>
-                </g>
-                <g style="fill:#fff; stroke:#ccc; stroke-width:1.5">
-                <circle cx="8" cy="6" r="3.5"/>
-                <circle cx="18" cy="16" r="3.5"/>
-                <circle cx="26" cy="26" r="3.5"/>
-                </g>
-            </svg>
+        <svg width="30px" height="30px" viewBox="0 0 20 20">
+            <g transform="translate(-8.000000, -8.000000)">
+                <path d="M12,17 L12,15 L14,15 L14,21 L12,21 L12,19 L8,19 L8,17 L12,17 Z M24,10 L28,10 L28,12 L24,12 L24,14 L22,14 L22,8 L24,8 L24,10 Z M17,24 L17,22 L19,22 L19,28 L17,28 L17,26 L8,26 L8,24 L17,24 Z M21,24 L28,24 L28,26 L21,26 L21,24 Z M8,10 L20,10 L20,12 L8,12 L8,10 Z M16,17 L28,17 L28,19 L16,19 L16,17 Z" fill="#999"></path>
+            </g>
+        </svg>
             &nbsp;&nbsp;
             {{ config('app.name') }}
         </a>
